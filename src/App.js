ed9afd7852b0f23454002a36acaa046de4e31098
Aashish Patil <aashish@appsdeployer.com>
2023-01-30 12:40:16 +0530

    four

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Navbara from './component/Navbar/Navbar';
+import First from './component/Part/First';
+import Second from './component/Part/second';
 
 function App() {
   return (
     <>
       <Navbara/>
+      <Routes>
+        <Route path='/' element={<First />} />
+        <Route path='/second' element={<Second />} />
+      </Routes>
     </>
   );
 }
